51bd5d8c7b7bbbf9bac0576b5ab110d5cb2b5916
Kenes Kuanyshov <kuanyshovk@gmail.com>
2021-07-25 20:37:51 +0600

    init commit

---
+++ Contants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtBearerTest
+{
+    public static class Constants1
+    {
+        public const string Issuer = "http://localhost:54218";
+        public const string Audience = Issuer;
+        public const string SecretKey = "this_is_secret_key_for_jwt_token_generation";
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtBearerTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "User11"),
+                new Claim(JwtRegisteredClaimNames.Email, "User11@mail.com")
+            };
+
+            byte[] secretBytes = Encoding.UTF8.GetBytes(Constants1.SecretKey);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var signingCredentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                Constants1.Issuer,
+                Constants1.Audience,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials);
+
+            var valueToken = new JwtSecurityTokenHandler().WriteToken(token);
+            ViewBag.Token = valueToken;
+            return View();
+        }
+    }
+}
